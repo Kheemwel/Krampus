123438d4e615ba9edb1b684fc028058fd8129c5a
Adner_Devila <116179168+A-Devss@users.noreply.github.com>
2023-10-26 10:26:36 +0800

    UI
    splash screen - main menu - joystick - options (on progress)

---
+++ script/player.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+
+@onready var joystick = %joystick
+
+var direction = Vector2.ZERO
+var speed = 70
+
+func _physics_process(delta):
+	move()
+
+func move():
+	var direction = joystick.posVector
+	if direction:
+		velocity = direction * speed
+	else:
+		velocity = Vector2(0,0)
+		
+	move_and_slide()
