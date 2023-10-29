2d712e99903de8cc108b7c52c9dbedc0c2dac028
Kimwel Beller <94892701+Kheemwel@users.noreply.github.com>
2023-10-29 09:57:53 +0800

    re-arranged files

---
+++ Scripts/SceneScripts/MainMenu.gd
@@ -0,0 +1,9 @@
+extends Control
+
+
+func _on_start_pressed():
+	get_tree().change_scene_to_file("res://Scenes/MapScenes/Stage 1/stage_1_living_dining_room.tscn")
+
+
+func _on_option_pressed():
+	pass # Replace with function body.
--- script/MainMenu.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-
-
-func _on_start_pressed():
-	get_tree().change_scene_to_file("res://scenes/main_level.tscn")
-
-
-func _on_option_pressed():
-	pass # Replace with function body.
