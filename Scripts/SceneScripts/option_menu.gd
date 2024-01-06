8e5047233651344b459bcc23402a8ff40c8ef2e6
Kimwel Beller <94892701+Kheemwel@users.noreply.github.com>
2024-01-06 22:43:03 +0800

    change options menu gui
    - custom gui for mute/unmute buttons
    - custom volume slider

--- Scripts/SceneScripts/option_menu.gd
+++ Scripts/SceneScripts/option_menu.gd
@@ -3,33 +3,25 @@ extends Control
 @onready var options : Dictionary = GameData.gamedata.options
 
 func _ready():
-	%MuteBGM.button_pressed = options.bgm_mute
-	%MuteSFX.button_pressed = options.sfx_mute
+	%BGMButton.button_pressed = options.bgm_mute
+	%SFXButton.button_pressed = options.sfx_mute
 	pass
-
-#func _on_option_pressed():
-#	set_previous_scene(get_tree().get_current_scene())
-#	get_tree().change_scene_to_file("res://Scenes/UI/option_menu.tscn")
-
-
-# Replace with function body.
 
 
 func _on_back_button_pressed():
 	get_tree().change_scene_to_file("res://Scenes/UI/MainMenu.tscn")
 
 
-
-
 func _on_tree_exiting():
 	GameData.gamedata.options = options
 	GameData.save_data()
 
-func _on_mute_bgm_toggled(button_pressed):
-	AudioServer.set_bus_mute(1, button_pressed)
-	options.bgm_mute = button_pressed
+
+func _on_bgm_button_toggled(toggled_on):
+	AudioServer.set_bus_mute(1, toggled_on)
+	options.bgm_mute = toggled_on
 
 
-func _on_mute_sfx_toggled(button_pressed):
-	AudioServer.set_bus_mute(2, button_pressed)
-	options.sfx_mute = button_pressed
+func _on_sfx_button_toggled(toggled_on):
+	AudioServer.set_bus_mute(2, toggled_on)
+	options.sfx_mute = toggled_on
